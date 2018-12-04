536622e8f4627fe03f700d92f2ecd4b1d5a0b637
ring19990528 <ring3884@gmail.com>
2018-12-04 16:32:32 +0800

    agetext,recycler

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.user.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 //CTRL+ALT+F快速鍵  Ctrl+alt+M     Ctrl+O override
@@ -10,6 +17,7 @@ public class MainActivity extends BasicActivity {
 
     public static final int RC_LOGIN = 100;
     boolean logon = false;
+    List<String> fruits = Arrays.asList("荔枝","蘋果","草莓");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +26,18 @@ public class MainActivity extends BasicActivity {
             Intent intent = new Intent(this,LoginActivity.class);
             startActivityForResult(intent,RC_LOGIN);
         }
+        //ListView
+        //listView();
+        //RecyclerView(可儲存資料)
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));//行列式清單
+        //recyclerView.setAdapter(new FruitAdapter());//在class新建一個class FruitAdapter 繼承RecyclerView
+        //data
+        List<String> fruits = Arrays.asList("荔枝","蘋果","草莓");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -27,7 +47,7 @@ public class MainActivity extends BasicActivity {
                 finish();
             }else{
                 logon = true;
-                if(user.isValid()) {
+                if(!user.isValid()) {
                     Intent intent = new Intent(this, NickNameActivity.class);
                     startActivity(intent);
                 }
