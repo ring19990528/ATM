5c067ad81ac35d57be99d2920b8f800dff8f6ae5
ring19990528 <ring3884@gmail.com>
2018-12-18 16:23:53 +0800

    setbackgroundcolor,settext

--- app/src/main/java/com/example/user/myapplication/AgeActivity.java
+++ app/src/main/java/com/example/user/myapplication/AgeActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -17,25 +18,26 @@ import org.w3c.dom.Text;
 public class AgeActivity extends BasicActivity {
 
     private EditText edage;
-    int[] numbers = {19,20,21,22,23,24,25};
+    int[] numbers = {17,18,19,20,21,22,23,24,25};
+    String[] rainbow = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        rainbow = getResources().getStringArray(R.array.rainbow);
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //TODO:set Adapter
         AgeAdapter adapter = new AgeAdapter();
         recyclerView.setAdapter(adapter);
+        edage = findViewById(R.id.ed_age);
     }
     public void next(View view){
-        edage = findViewById(R.id.ed_age);
         int age = Integer.parseInt(edage.getText().toString());
         user.setAge(age);
         Intent intent = new Intent(this,GenderActivity.class);
         startActivity(intent);
-
     }
     public void back(View view){
         finish();
@@ -49,19 +51,25 @@ public class AgeActivity extends BasicActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull AgeAdapter.AgeHolder holder, int position) {
+        public void onBindViewHolder(@NonNull AgeAdapter.AgeHolder holder, final int position) {
             holder.ageText.setText(numbers[position] + "");
-            if(numbers[position] == 19){
-                holder.ageText.setTextColor(Color.RED);
-            }
+            holder.itemView.setBackgroundColor(Color.parseColor(rainbow[position%7]));
+//            if(numbers[position] == 19){
+//                holder.ageText.setTextColor(Color.RED);
+//            }
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("AgeActivity", "onClick: " + numbers[position]);
+                    edage.setText(numbers[position] + "");
+                }
+            });
         }
 
         @Override
         public int getItemCount() {
             return numbers.length;
         }
-
-
 
     class  AgeHolder extends RecyclerView.ViewHolder{
             TextView ageText;
